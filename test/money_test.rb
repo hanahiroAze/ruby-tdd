ee6a3ae0d2f86d4116918245e64aabd12686a4e7
hanahiroAze <hiroyuki.hanai@gmail.com>
2018-01-30 01:54:55 +0900

    feat: francのテスト追加

--- test/money_test.rb
+++ test/money_test.rb
@@ -1,16 +1,21 @@
 require 'minitest/autorun'
-require './lib/dollar.rb'
+require './lib/money.rb'
 
-class DollarTest < Minitest::Test
+class MoneyTest < Minitest::Test
   def test_times
     five = Dollar.new(5)
     assert_equal Dollar.new(10), five.times(2)
     assert_equal Dollar.new(15), five.times(3)
+    five = Franc.new(5)
+    assert_equal Franc.new(10), five.times(2)
+    assert_equal Franc.new(15), five.times(3)
   end
 
   def test_equals
     assert_equal(true, Dollar.new(5).equals(Dollar.new(5)))
     assert_equal(false, Dollar.new(5).equals(Dollar.new(6)))
+    assert_equal(true, Franc.new(5).equals(Franc.new(5)))
+    assert_equal(false, Franc.new(5).equals(Franc.new(6)))
   end
 end
 

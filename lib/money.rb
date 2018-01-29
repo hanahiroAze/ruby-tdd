80ce8a91ba61e5337cbdb9a2fb24f3ec64591caf
hanahiroAze <hiroyuki.hanai@gmail.com>
2018-01-30 02:01:31 +0900

    feat:Francクラスの実装

--- lib/money.rb
+++ lib/money.rb
@@ -19,3 +19,25 @@ class Dollar
     self.equals(target)
   end
 end
+
+class Franc
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def times(times)
+    Franc.new(@amount * times)
+  end
+
+  def equals(obj)
+    obj.amount == @amount
+  end
+
+  def amount
+    @amount
+  end
+
+  def ==(target)
+    self.equals(target)
+  end
+end

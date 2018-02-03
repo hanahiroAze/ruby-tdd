d5809d012cbcc542e7e1a5800c647f75f266046e
hanahiroAze <hiroyuki.hanai@gmail.com>
2018-02-03 19:28:15 +0900

    refactor: Moneyクラスに共通のメソッドを移動

--- lib/money.rb
+++ lib/money.rb
@@ -2,40 +2,28 @@ class Money
  def initialize(amount)
    @amount = amount
  end
+
+ def equals(obj)
+    obj.amount == @amount
+ end
+
+ def amount
+  @amount
+ end
+
+ def ==(target)
+    self.equals(target)
+ end
 end
 
 class Dollar < Money 
   def times(times)
     Dollar.new(@amount * times)
   end
-
-  def equals(obj)
-    obj.amount == @amount
-  end
-
-  def amount
-    @amount
-  end
-
-  def ==(target)
-    self.equals(target)
-  end
 end
 
 class Franc < Money
   def times(times)
     Franc.new(@amount * times)
-  end
-
-  def equals(obj)
-    obj.amount == @amount
-  end
-
-  def amount
-    @amount
-  end
-
-  def ==(target)
-    self.equals(target)
   end
 end

97eac58b42d220819f5def3df8037a02dd91f1a3
hanahiroAze <hiroyuki.hanai@gmail.com>
2018-02-03 19:40:19 +0900

    refactor: initializeはそれぞれのクラスにもたせておく

--- lib/money.rb
+++ lib/money.rb
@@ -1,28 +1,32 @@
 class Money
- def initialize(amount)
-   @amount = amount
- end
-
  def equals(obj)
     obj.amount == @amount
  end
 
  def amount
-  @amount
+   @amount
  end
 
  def ==(target)
-    self.equals(target)
+   self.equals(target)
  end
 end
 
 class Dollar < Money 
+  def initialize(amount)
+    @amount = amount
+  end
+  
   def times(times)
     Dollar.new(@amount * times)
   end
 end
 
 class Franc < Money
+  def initialize(amount)
+    @amount = amount
+  end
+
   def times(times)
     Franc.new(@amount * times)
   end

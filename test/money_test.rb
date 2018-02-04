88cc5a146cc426cbff06cc65d6cb986492d21789
hanahiroAze <hiroyuki.hanai@gmail.com>
2018-02-04 20:24:44 +0900

    refactor: 過剰なテストの削除

--- test/money_test.rb
+++ test/money_test.rb
@@ -14,8 +14,6 @@ class MoneyTest < Minitest::Test
   def test_equals
     assert_equal(true, Money.dollar(5).equals(Money.dollar(5)))
     assert_equal(false, Money.dollar(5).equals(Money.dollar(6)))
-    assert_equal(true, Money.franc(5).equals(Money.franc(5)))
-    assert_equal(false, Money.franc(5).equals(Money.franc(6)))
     assert_equal(false, Money.franc(5).equals(Money.dollar(5)))
     assert_equal(true, Money.new(5, "USD").equals(Money.new(5, "USD")))
     assert_equal(true, Money.new(5, "CHF").equals(Money.new(5, "CHF")))

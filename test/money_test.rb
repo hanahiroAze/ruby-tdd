bf7ab3002e88d53f88b40c306d096b7f8f39dbb0
hanahiroAze <hiroyuki.hanai@gmail.com>
2018-02-04 20:39:40 +0900

    refactor: timesのテスト削除

--- test/money_test.rb
+++ test/money_test.rb
@@ -2,15 +2,6 @@ require 'minitest/autorun'
 require './lib/money.rb'
 
 class MoneyTest < Minitest::Test
-  def test_times
-    five = Money.dollar(5)
-    assert_equal Money.dollar(10), five.times(2)
-    assert_equal Money.dollar(15), five.times(3)
-    five = Money.franc(5)
-    assert_equal Money.franc(10), five.times(2)
-    assert_equal Money.franc(15), five.times(3)
-  end
-
   def test_equals
     assert_equal(true, Money.dollar(5).equals(Money.dollar(5)))
     assert_equal(false, Money.dollar(5).equals(Money.dollar(6)))
